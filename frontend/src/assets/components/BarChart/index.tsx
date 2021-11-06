9fbd161c1b1ee516de89063a5ef3a080dbcf42dc
ricardoalvescarvalho <ricardo.alves.carvalho@gmail.com>
2021-11-06 18:24:35 -0300

    Err Barchart fixed

--- frontend/src/assets/components/BarChart/index.tsx
+++ frontend/src/assets/components/BarChart/index.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
 import axios from  'axios';
 import { BASE_URL } from 'utils/requests';
-import { SaleSuccess, SaleSum } from 'types/sale';
+import { SaleSuccess } from 'types/sale';
 import { round } from 'utils/format';
 
 type SeriesData = {
